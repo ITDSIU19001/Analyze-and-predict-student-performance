30128b4cff6ea7837ff02c6f754333c0aaebe802
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2023-04-21 10:04:35 +0700

    Revert "Update Test.py"
    This reverts commit ada60a560b41f7e24df89028ff868915979cecc2.

--- Test.py
+++ Test.py
@@ -43,7 +43,7 @@ with col1:
 
 # add a centered title to the second column
 with col2:
-    st.title("Student Performance Prediction Systems")
+    st.title("IT Student Scores")
 
 
 # Load the raw data
