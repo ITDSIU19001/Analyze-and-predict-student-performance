d105f523baf9c529c3031d1f5368b108bcc16b8f
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2023-05-20 17:12:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ if tabs == "Dashboard":
         course_data = df[course].dropna()
 
         # Generate comment and summary statistics
-        if len(course_data) > 0:
+        if len(course_data) > 2:
             st.write("Course:", course, " of ", school, " student")
             st.write(generate_comment(course_data.median()))
         else:
