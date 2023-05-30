2aaec44c39f3700ca6703e7596b88a1091ad90e4
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2023-05-30 23:57:25 +0700

    Update function.py

--- function.py
+++ function.py
@@ -45,7 +45,8 @@ def process_data_per(raw_data):
     pivot_df = pivot_df.dropna(thresh=50, axis=1)
     pivot_df = pivot_df.rename(columns=lambda x: x.strip())
     # Drop unnecessary columns
-    pivot_df.replace('WH', value=np.nan, regex=True)
+    
+    pivot_df.replace('WH', np.nan, inplace=True)
     pivot_df.iloc[:, 1:] = pivot_df.iloc[:, 1:].apply(pd.to_numeric)
     # Merge with the XepLoaiNH column
     # df = pd.merge(pivot_df, raw_data[['MaSV', 'XepLoaiNH']], on='MaSV')
