d290be3dbe4b23af80e8870b8bd9fff889273f12
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2023-06-04 16:42:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -299,15 +299,21 @@ elif tabs == "Predict":
     # Filter students who have a Result value of "late"
     df_late = predict
     
-    def clear_button():
-        global MaSV
-        MaSV = ""
+    clear_text_input_js = """
+    <script>
+    function clearTextInput() {
+        var input = document.getElementById("text-input");
+        input.value = "";
+    }
+    </script>
+    """
+
+    # Add the JavaScript code to the Streamlit app
+    st.markdown(clear_text_input_js, unsafe_allow_html=True)
 
     # Create the text input and clear button
-    MaSV = st.text_input("Enter Student ID:")
-
-    if st.button("Clear"):
-        clear_button()
+    MaSV = st.text_input("Enter Student ID:", key="text-input")
+    if st.button("Clear", on_click="clearTextInput"):
 
     # Create a clear button
     
