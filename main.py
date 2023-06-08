5ed76c0a65e7cac4d6f0839aeb1eac5bb170d42d
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2023-06-08 16:53:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -167,7 +167,7 @@ if tabs == "Dashboard":
     fig = px.box(dfl)
     # Set the title
     fig.update_layout(title="Boxplot for Each Column")
-    fig.update_layout(width=1300)
+    fig.update_layout(width=1250)
     st.plotly_chart(fig)
 
     # Select course dropdown
