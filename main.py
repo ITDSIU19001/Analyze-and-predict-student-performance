4b80b4ce71d337cc072a663c582c96b139082d3d
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2023-06-08 17:04:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -160,8 +160,15 @@ if tabs == "Dashboard":
 
     # Drop NaN columns
     df.dropna(axis=1, thresh=1, inplace=True)
-    
-    
+    show_boxplot = st.checkbox("Show Boxplot")
+    if show_boxplot:
+        dfl = df.iloc[:, :-3].apply(pd.to_numeric)
+        # Use Plotly Express to create a boxplot for each column
+        fig = px.box(dfl)
+        # Set the title
+        fig.update_layout(title="Boxplot for Each Column")
+        fig.update_layout(width=1250)
+        st.plotly_chart(fig)
     # Select course dropdown
     options = df.columns[:-3]
     course_data_dict = {course: df[course].dropna() for course in options}
@@ -258,13 +265,7 @@ if tabs == "Dashboard":
             fig.update_layout(height=400, width=400)
             st.plotly_chart(fig)
 
-    dfl = df.iloc[:, :-3].apply(pd.to_numeric)
-    # Use Plotly Express to create a boxplot for each column
-    fig = px.box(dfl)
-    # Set the title
-    fig.update_layout(title="Boxplot for Each Column")
-    fig.update_layout(width=1250)
-    st.plotly_chart(fig)
+
 
 
 
