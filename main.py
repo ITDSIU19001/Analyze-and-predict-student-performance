51965ee968928dde383205caa37e45297e2f81bd
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2023-06-10 03:00:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -237,8 +237,8 @@ if tabs == "Dashboard":
         if show_boxplot:
             fig = px.box(new_df)
             fig1 = px.box(new_dfa)
-            fig.update_layout(title="Boxplot of " + major + school + " student at" + year)
-            fig1.update_layout(title="Boxplot of " + major + school + " student at" + year_a)
+            fig.update_layout(title="Boxplot of " + major + school + " student at " + year)
+            fig1.update_layout(title="Boxplot of " + major + school + " student at " + year_a)
             # fig.update_layout(width=1250)
             # fig1.update_layout(width=1250)
             st.plotly_chart(fig,use_container_width=True)
