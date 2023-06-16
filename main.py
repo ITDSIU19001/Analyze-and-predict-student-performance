60e1a1f304c01b2ea6dc50c86bfd3a32a3c8f2d8
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2023-06-16 23:23:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -188,16 +188,7 @@ if tabs == "Dashboard":
     
     df = filter_dataframe(df, "Year", year)
 
-    data1=raw_data[["MaSV","DTBTK"]]
-    data1=data1.drop_duplicates()
-    data1["MaSV_school"] = data1["MaSV"].str.slice(2, 4)
-    data1["Major"] = data1["MaSV"].str.slice(0, 2)
-    data1["Year"] = 2000 + data1["MaSV"].apply(get_year)
-    data1["Year"] = data1["Year"].astype(str)
-    data1 = filter_dataframe(data1, "Major", major)
-    data1 = filter_dataframe(data1, "MaSV_school", additional_selection)
-    fig = px.box(data1["DTBTK"])
-    st.plotly_chart(fig,use_container_width=True)
+    st.write(df["DTBTK"])
 
 
 
