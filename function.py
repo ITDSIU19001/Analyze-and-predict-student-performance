fc58e17f8e22ac89aab5aafb2f729ff6c7f93116
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2023-06-17 04:30:02 +0700

    Update function.py

--- function.py
+++ function.py
@@ -45,7 +45,7 @@ def process_data(raw_data):
     df["Major"] = df["MaSV"].str.slice(0, 2)
     df["Year"] = 2000 + df["MaSV"].apply(get_year)
     df["Year"] = df["Year"].astype(str)
-    df=pd.merge(raw_data[["MaSV","DTBTK"]], df,on="MaSV")
+    df=pd.merge( df,raw_data[["MaSV","DTBTK"]].drop_duplicates(),on="MaSV")
     df = df.drop(columns="MaSV")
     
 
