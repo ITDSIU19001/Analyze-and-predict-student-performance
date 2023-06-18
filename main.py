fd3a65e9f8163203317f4e786f8a4f749a180e30
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2023-06-19 00:01:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -258,7 +258,9 @@ if tabs == "Dashboard":
     valid_courses = [
         course for course, data in course_data_dict.items() if len(data) > 1
     ]
-    if (year != "All" and year_a != " ") or (school != "All" and additional_selection != " "):
+    if (year_a == " " and additional_selection == " ") :
+        valid_courses=np.concatenate((valid_courses, "All"), axis=None) 
+    else ((year != "All" and year_a != " ") or (school != "All" and additional_selection != " ")):
         dfac=new_dfa.columns[:-4].tolist()
         common_elements = np.intersect1d(valid_courses, dfac)
         # Merge arrays with common elements
@@ -266,8 +268,7 @@ if tabs == "Dashboard":
 
         # Filter merged array to retain only common elements
         valid_courses = np.intersect1d(merged_array, common_elements)
-    else:
-        valid_courses=np.concatenate((valid_courses, "All"), axis=None)
+
 
     if len(valid_courses) > 5:
         course = st.selectbox("Select a course:", valid_courses)
