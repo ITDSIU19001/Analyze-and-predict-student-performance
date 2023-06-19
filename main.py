99c76768d8520254f30c4a27a5fe772fa31548c6
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2023-06-19 16:28:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def color_cell(val):
     elif val == "late":
         color = "red"
     else:
-        color = st.get_option("theme.primaryColor")  # Dynamic theme color
+        color = st.get_option("theme.textColor")  # Dynamic theme color
         
     return f"color: {color};"
 
