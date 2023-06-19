a0b0cc751f9c15fc38c512fcdd4ad0a01759ebed
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2023-06-20 00:06:08 +0700

    Update function.py

--- function.py
+++ function.py
@@ -295,6 +295,22 @@ def predict_one_student(raw_data, student_id):
         frequencies_percentage = (counts / total_count) * 100
         # create a line chart using plotly
         fig1 = go.Figure()
+        # fig1.add_trace(
+        #     go.Histogram(
+        #         x=values,
+        #         nbinsx=40,
+        #         name=student_id,
+        #         marker=dict(color="rgba(50, 100, 200, 0.7)"),
+        #     )
+        # )
+
+        # # set the chart title and axis labels
+        # fig1.update_layout(
+        #     title="Histogram for student {}".format(student_id),
+        #     xaxis_title="Value",
+        #     yaxis_title="Frequency",
+        #     width=500,
+        # )
         fig1.add_trace(go.Scatter(x=bins[:-1], y=frequencies_percentage, mode='lines', name='Frequency'))
 
         fig1.update_layout(
