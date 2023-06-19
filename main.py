99fb007ed03d79896f3c9b995501693cea09fde9
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2023-06-19 14:52:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -299,6 +299,15 @@ if tabs == "Dashboard":
         counts, bins = np.histogram(course_data,bins=np.arange(0, 110, 10))
         total_count = len(course_data)
         frequencies_percentage = (counts / total_count) * 100
+        grade_bins = [f'{bins[i]}-{bins[i+1]}' for i in range(len(bins) - 1)]
+
+        # Create a DataFrame with the updated 'Grade' column and frequencies_percentage
+        df = pd.DataFrame({'Grade': grade_bins, 'Grading percentage': frequencies_percentage})
+        df['Grading percentage'] = df['Grading percentage'].map(lambda x: '{:.2f}'.format(x))
+        
+        st.table(df)
+
+    with col2:
 
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=bins[:-1], y=frequencies_percentage, mode='lines', name='Frequency'))
@@ -311,14 +320,6 @@ if tabs == "Dashboard":
             width=400,
         )
         st.plotly_chart(fig,use_container_width=True)
-    with col2:
-        grade_bins = [f'{bins[i]}-{bins[i+1]}' for i in range(len(bins) - 1)]
-
-        # Create a DataFrame with the updated 'Grade' column and frequencies_percentage
-        df = pd.DataFrame({'Grade': grade_bins, 'Grading percentage': frequencies_percentage})
-        df['Grading percentage'] = df['Grading percentage'].map(lambda x: '{:.2f}'.format(x))
-        
-        st.table(df)
 
         
     with col3:
@@ -389,6 +390,16 @@ if tabs == "Dashboard":
             counts, bins = np.histogram(course_data_filtered,bins=np.arange(0, 110, 10))
             total_count = len(course_data_filtered)
             frequencies_percentage = (counts / total_count) * 100
+            grade_bins = [f'{bins[i]}-{bins[i+1]}' for i in range(len(bins) - 1)]
+
+            # Create a DataFrame with the updated 'Grade' column and frequencies_percentage
+            df1 = pd.DataFrame({'Grade': grade_bins, 'Grading percentage': frequencies_percentage})
+            df1['Grading percentage'] = df1['Grading percentage'].map(lambda x: '{:.2f}'.format(x))
+            
+            st.table(df1)
+
+
+        with col2:
 
             fig = go.Figure()
             fig.add_trace(go.Scatter(x=bins[:-1], y=frequencies_percentage, mode='lines', name='Frequency'))
@@ -401,14 +412,6 @@ if tabs == "Dashboard":
                 width=400,
             )
             st.plotly_chart(fig,use_container_width=True)
-        with col2:
-            grade_bins = [f'{bins[i]}-{bins[i+1]}' for i in range(len(bins) - 1)]
-
-            # Create a DataFrame with the updated 'Grade' column and frequencies_percentage
-            df1 = pd.DataFrame({'Grade': grade_bins, 'Grading percentage': frequencies_percentage})
-            df1['Grading percentage'] = df1['Grading percentage'].map(lambda x: '{:.2f}'.format(x))
-            
-            st.table(df1)
 
         with col3:
             fig = go.Figure()
