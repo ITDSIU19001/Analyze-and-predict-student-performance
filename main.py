50f6dee9b01de33a298bb9217728fe6ffd020a11
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2023-06-19 14:42:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -179,19 +179,19 @@ if tabs == "Dashboard":
 
     # Second selectbox in the second column
     with col2:
-        year_a = " "
+
         if year != "All" and additional_selection == " ":
             year_list = [x for x in all_values_year if x != "All" and x != year]
             year_list = np.concatenate([[" "], year_list])
             year_a = st.selectbox("Select another year for comparisons:", year_list)
-
+        elif year!="All" and additional_selection != " ":
+            year_a=year
             if year_a != " ":
                 dfa = filter_dataframe(dfa, "Year", year_a)
                 dfa.dropna(axis=1, thresh=1, inplace=True)
     
     df = filter_dataframe(df, "Year", year)
 
-    st.write(year_a)
 
     new1_df = df.DTBTK
     new1_dfa = dfa.DTBTK
