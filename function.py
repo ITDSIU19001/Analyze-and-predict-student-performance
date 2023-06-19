edb17a517cc29ec9adda4cba23a998ad46bc12bb
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2023-06-19 15:50:14 +0700

    Update function.py

--- function.py
+++ function.py
@@ -293,9 +293,6 @@ def predict_one_student(raw_data, student_id):
         grade_bins = [f'{bins[i]}-{bins[i+1]}' for i in range(len(bins) - 1)]
         total_count = len(selected_row)
         frequencies_percentage = (counts / total_count) * 100
-        df1 = pd.DataFrame({'Grade': grade_bins, 'Grading percentage': frequencies_percentage})
-        df1['Grading percentage'] = df1['Grading percentage'].map(lambda x: '{:.2f}'.format(x))
-        st.table(df1)
         # create a line chart using plotly
         fig1 = go.Figure()
         # fig1.add_trace(
