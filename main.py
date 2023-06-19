7e8780a33b858faf0191dc6c0c25438535e3eca6
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2023-06-20 00:26:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -199,8 +199,7 @@ if tabs == "Dashboard":
     df = filter_dataframe(df, "Year", year)
 
     new1_df = df.DTBTK
-    if dfa != None:
-        new1_dfa = dfa.DTBTK
+    new1_dfa = dfa.DTBTK
     show_boxplot1(new1_df, new1_dfa, major, school, year, additional_selection="", year_a="")
     
     # Drop NaN columns
